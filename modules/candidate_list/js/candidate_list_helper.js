fe63a9c3a4bed820f564927b60883616d50f8d44
llevitis <llevitis@cbrain.mcgill.ca>
2017-11-26 22:31:46 -0500

    resolving js files

--- modules/candidate_list/js/candidate_list_helper.js
+++ modules/candidate_list/js/candidate_list_helper.js
@@ -1,4 +1,43 @@
 /*global document: false, $: false, window: false, unescape: false, Option: false,isElementsSet, alert*/
+<<<<<<< 3a82da2167300510bdffb3fc9a433e97ade0ee28
+=======
+
+loris.hiddenHeaders = ['Consent To Study'];
+
+function checkAccessProfileForm() {
+    'use strict';
+    var form = document.accessProfileForm;
+    if (form.candID.value === "") {
+        alert("You must enter a DCC-ID");
+        form.candID.focus();
+        return false;
+    }
+    if (form.PSCID.value === "") {
+        alert("You must enter a PSCID");
+        form.PSCID.focus();
+        return false;
+    }
+
+    $.get(loris.BaseURL + "/AjaxHelper.php?Module=candidate_list&script=validateProfileIDs.php&candID=" + form.candID.value + "&PSCID=" + form.PSCID.value ,
+        function(data)
+        {
+            //ids are valid, submit accessProfileForm form
+            if (data==1) {
+                $( "#accessProfileForm" ).unbind('submit.formValidation');
+                form.submit();
+            }
+            else {
+                //display error message
+
+                alert("DCCID or PSCID is not valid");
+            }
+        }
+    );
+
+    return false;
+}
+
+>>>>>>> working on modifying the appearance of consent for study_consent being set to no
 function hideFilter(obj) {
     'use strict';
 
